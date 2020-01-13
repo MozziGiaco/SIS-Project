a85be6fc2fe17c9ed966cfc1742be87abe0e9034
Ethan Griffin <ethan.griffin@mustangs>
2020-01-13 11:47:30 -0700

    Made Student Pojo

--- SIS Project/src/Student.java
+++ SIS Project/src/Student.java
@@ -1,10 +1,26 @@
 
 public class Student
 	{
+		
+		
+		protected String firstName;
+		protected String lastName;
+		
+		protected Class[] classes;
+		protected double gpa;
+		
 
-		public static void main(String[] args)
+		public Student(String firstName, String lastName, Class[] classes)
 			{
 
+				this.firstName = firstName;
+				this.lastName = lastName;
+				this.classes = classes;
+				
+				//TODO make generate GPA based on the classes
+				this.gpa = 0.00;
+				
+				
 			}
 
 	}
