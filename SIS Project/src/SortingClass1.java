5b9ec0c0647e80f61e3216aba3d5984baae57353
danielle.failor <danielle.failor@MU-C1-02.mullen.pvt.k12.co.us>
2020-01-21 11:31:36 -0700

    good

--- SIS Project/src/SortingClass1.java
+++ SIS Project/src/SortingClass1.java
@@ -5,8 +5,18 @@ public class SortingClass1 implements Comparator<Student>
 		@Override
 		public int compare(Student s1, Student s2)
 			{
-				return s1.classes[0].getName().toLowerCase().compareTo(s2.classes[0].getName().toLowerCase());
+				if(s1.classes[0].getName().toLowerCase().equals(s2.classes[0].getName().toLowerCase()))
+					{
+						return s1.classes[0].getName().toLowerCase().compareTo(s2.classes[0].getName().toLowerCase()) 
+								+s1.getLastName().toLowerCase().compareTo(s2.getLastName().toLowerCase());
+					}
+				else
+					{
+						return s1.classes[0].getName().toLowerCase().compareTo(s2.classes[0].getName().toLowerCase()) ;
+					}
+				
 				//Compares students classes
 			
 			}
+		
 	}
