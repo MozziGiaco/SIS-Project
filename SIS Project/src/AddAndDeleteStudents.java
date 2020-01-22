b3a6e04f6ff69c5bc4fee09934aa8c9f48065de3
Owner <Owner@10.0.0.182>
2020-01-21 18:17:22 -0700

    Finished Fixing errors in AddingAndDeleting Students

--- SIS Project/src/AddAndDeleteStudents.java
+++ SIS Project/src/AddAndDeleteStudents.java
@@ -4,6 +4,18 @@ import java.io.File;
 
 public class AddAndDeleteStudents
 	{
+		static Scanner intInput = new Scanner(System.in);
+		static Scanner strInput = new Scanner(System.in);
+		static int userChoice;
+		static String class1;
+		static String class2;
+		static String class3;
+		static int i;
+		static String fullName;
+		static String firstName;
+		static String lastName;
+		static String firstName1;
+		static String lastName2;
 
 		// TODO try to implement a time delay in certain areas to enhance the program,
 		// and mimic the thinking of a computer when running the program to either add
@@ -20,8 +32,7 @@ public class AddAndDeleteStudents
 				System.out.println("(1) Add a Student");
 				System.out.println("(2) Delete a Student");
 
-				Scanner intInput = new Scanner(System.in);
-				int userChoice = intInput.nextInt();
+				userChoice = intInput.nextInt();
 
 				if (userChoice == 1)
 					{
@@ -49,8 +60,7 @@ public class AddAndDeleteStudents
 				// having the user enter the full name of the student they want to add
 				System.out.println("Please enter the FULL name of the student you want to add.");
 
-				Scanner stringInput1 = new Scanner(System.in);
-				String fullName = stringInput1.nextLine();
+				fullName = strInput.nextLine();
 
 				// telling the user the choice of classes for their new student
 				System.out.println("Keep in mind, there are three classes available for the new student...");
@@ -65,22 +75,19 @@ public class AddAndDeleteStudents
 				System.out.println("Written as A+, D-, F, B, etc.");
 				System.out.println("Ex. English A-");
 
-				Scanner stringInput3 = new Scanner(System.in);
-				String class1 = stringInput3.nextLine();
+				class1 = strInput.nextLine();
 
 				// having the user enter the second class and corresponding grade for their new
 				// student
 				System.out.println("Please enter the second class and corresponding grade for the new student.");
 
-				Scanner stringInput4 = new Scanner(System.in);
-				String class2 = stringInput4.nextLine();
+				class2 = strInput.nextLine();
 
 				// having the user enter the third and final class and corresponding grade for
 				// their new student
 				System.out.println("Please enter the third class and corresponding grade for the new student.");
 
-				Scanner stringInput5 = new Scanner(System.in);
-				String class3 = stringInput5.nextLine();
+				class3 = strInput.nextLine();
 
 				Class[] classArray = new Class[3];
 
@@ -96,7 +103,7 @@ public class AddAndDeleteStudents
 						// Line to integrate the printing of the list of students
 						Roster.load();
 
-						for (int i = 0; i < Roster.roster.size(); i++)
+						for (i = 0; i < Roster.roster.size(); i++)
 							{
 								// code to either add or delete students from the 'class'
 								System.out.println("Add or Delete a Student");
@@ -104,14 +111,13 @@ public class AddAndDeleteStudents
 								System.out.println("(1) Add a Student");
 								System.out.println("(2) Delete a Student");
 
-								Scanner intInput = new Scanner(System.in);
-								int userChoice = intInput.nextInt();
+								userChoice = intInput.nextInt();
 
 								if (userChoice == 1)
 									{
 										System.out.println("You chose to add a student!");
 										// Line to integrate the printing of the list of students
-										for (int i = 0; i < Roster.getRoster().size(); i++)
+										for (i = 0; i < Roster.getRoster().size(); i++)
 											{
 												System.out.println(Roster.getRoster().get(i).getFullName());
 											}
@@ -120,15 +126,13 @@ public class AddAndDeleteStudents
 										System.out
 												.println("Please enter the first name of the student you want to add.");
 
-										Scanner stringInput1 = new Scanner(System.in);
-										String firstName = stringInput1.nextLine();
+										firstName = strInput.nextLine();
 
 										// having the user enter the last name of the student they want to add
 										System.out
 												.println("Please enter the last name of the student you want to add.");
 
-										Scanner stringInput2 = new Scanner(System.in);
-										String lastName = stringInput2.nextLine();
+										lastName = strInput.nextLine();
 
 										// telling the user the choice of classes for their new student
 										System.out.println(
@@ -145,24 +149,21 @@ public class AddAndDeleteStudents
 										System.out.println("Written as A+, D-, F, B, etc.");
 										System.out.println("Ex. English A-");
 
-										Scanner stringInput3 = new Scanner(System.in);
-										String class1 = stringInput3.nextLine();
+										class1 = strInput.nextLine();
 
 										// having the user enter the second class and corresponding grade for their new
 										// student
 										System.out.println(
 												"Please enter the second class and corresponding grade for the new student.");
 
-										Scanner stringInput4 = new Scanner(System.in);
-										String class2 = stringInput4.nextLine();
+										class2 = strInput.nextLine();
 
 										// having the user enter the third and final class and corresponding grade for
 										// their new student
 										System.out.println(
 												"Please enter the third class and corresponding grade for the new student.");
 
-										Scanner stringInput5 = new Scanner(System.in);
-										String class3 = stringInput5.nextLine();
+										class3 = strInput.nextLine();
 
 										// Figure out how to integrate GPA
 
@@ -173,7 +174,7 @@ public class AddAndDeleteStudents
 										System.out.println("You chose to delete a student.");
 
 										// Line to integrate the printing of the list of students
-										for (int i = 0; i < Roster.getRoster().size(); i++)
+										for (i = 0; i < Roster.getRoster().size(); i++)
 											{
 												System.out.println(Roster.getRoster().get(i).getFullName());
 											}
@@ -182,15 +183,13 @@ public class AddAndDeleteStudents
 										System.out.println(
 												"Please enter the first name of the student you wish to take off the roster.");
 
-										Scanner stringInput6 = new Scanner(System.in);
-										String firstName1 = stringInput6.nextLine();
+										firstName1 = strInput.nextLine();
 
 										// having the user enter the *last name* of the student they wish to delete
 										System.out.println(
 												"Please enter the last name of the student you wish to take off the roster.");
 
-										Scanner stringInput7 = new Scanner(System.in);
-										String lastName2 = stringInput7.nextLine();
+										lastName2 = strInput.nextLine();
 
 										// TODO make sure the program can scan through the roster in order to ensure
 										// that the name the user entered to delete is actually a 'real' name in said
@@ -207,8 +206,7 @@ public class AddAndDeleteStudents
 								System.out.println(
 										"Please enter the FULL name of the student you wish to take off the roster.");
 
-								Scanner stringInput6 = new Scanner(System.in);
-								String firstName1 = stringInput6.nextLine();
+								firstName1 = strInput.nextLine();
 
 								// TODO make sure the program can scan through the roster in order to ensure
 								// that the name the user entered to delete is actually a 'real' name in said
@@ -220,10 +218,10 @@ public class AddAndDeleteStudents
 								// code to actually remove
 //		    }
 
-//		    else
-//		    {
-//		    	System.out.println("I'm sorry, that name isn't in the database. Please try again.");
-//		    }
+								/*
+								 * else { System.out.
+								 * println("I'm sorry, that name isn't in the database. Please try again."); }
+								 */
 							}
 					}
 
